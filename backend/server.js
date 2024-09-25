1c194637581ec1d75f63135386d3e03af39ced7e
tamayo2 <juan.tamayo3@utp.edu.co>
2024-09-24 23:19:03 -0500

    commit inicial

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
 import express from 'express';
 
-const app
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.listen(5000, () => {
+  console.log('Server at http://localhost:5000');
+});
