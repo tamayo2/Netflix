20cf7655f95692d3a620d812e5cc034c093b0f7b
tamayo2 <juan.tamayo3@utp.edu.co>
2024-10-07 10:22:48 -0500

    finish backend-server

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,38 @@
-import express from 'express';
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
-app.get('/api/signup', (req, res) => {
-  res.send('Signup route');
-});
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
-app.get('/api/login', (req, res) => {
-    res.send('Login route');
-});
+app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser());
 
-app.get('/api/logout', (req, res) => {
-    res.send('Logout route');
-});
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
-app.listen(5000, () => {
-  console.log('Server at http://localhost:5000');
+if (ENV_VARS.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log("Server started at http://localhost:" + PORT);
+    connectDB();
 });
